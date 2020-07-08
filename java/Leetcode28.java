0efbc279bb582e4ee20d01209f37e92e7c2f8521
Sihw <sysuwsh@gmail.com>
2020-07-09 01:50:17 +0800

    Update Leetcode28.java

--- java/Leetcode28.java
+++ java/Leetcode28.java
@@ -1,46 +1,46 @@
-//public class Leetcode28 {
-//    public static void main(String[] args) {
-//
-//    }
-//}
-//
-//class Solution {
-//    public int strStr(String haystack, String needle) {
-//        if (needle.length() == 0)
-//            return 0;
-//        int i = 0;
-//        int j = 0;
-//        int[] next = buildNext(needle);
-//        while (i < haystack.length() && j < needle.length()) {
-//            if (j < 0 || needle.charAt(j) == haystack.charAt(i)) {
-//                ++i;
-//                ++j;
-//            }
-//            else {
-//                j = next[j];
-//            }
-//        }
-//        if (j == needle.length())
-//            return i - j;
-//        else
-//            return -1;
-//    }
-//
-//    public int[] buildNext(String p){
-//        int[] res = new int[p.length()];
-//        res[0] = -1;
-//        int t = res[0];
-//        int j = 0;
-//        while (j < p.length() - 1) {
-//            if (t < 0 || p.charAt(t) == p.charAt(j)) {
-//                ++t;
-//                ++j;
-//                res[j] = t;
-//            }
-//            else {
-//                t = res[t];
-//            }
-//        }
-//        return res;
-//    }
-//}
+public class Leetcode28 {
+   public static void main(String[] args) {
+
+   }
+}
+
+class Solution {
+   public int strStr(String haystack, String needle) {
+       if (needle.length() == 0)
+           return 0;
+       int i = 0;
+       int j = 0;
+       int[] next = buildNext(needle);
+       while (i < haystack.length() && j < needle.length()) {
+           if (j < 0 || needle.charAt(j) == haystack.charAt(i)) {
+               ++i;
+               ++j;
+           }
+           else {
+               j = next[j];
+           }
+       }
+       if (j == needle.length())
+           return i - j;
+       else
+           return -1;
+   }
+
+   public int[] buildNext(String p){
+       int[] res = new int[p.length()];
+       res[0] = -1;
+       int t = res[0];
+       int j = 0;
+       while (j < p.length() - 1) {
+           if (t < 0 || p.charAt(t) == p.charAt(j)) {
+               ++t;
+               ++j;
+               res[j] = t;
+           }
+           else {
+               t = res[t];
+           }
+       }
+       return res;
+   }
+}
